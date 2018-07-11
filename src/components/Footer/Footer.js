0c44c3423133335b8c4c831c47c1e3230412bc8e
user_name <miguel.gray95@gmail.com>
2018-07-10 20:17:17 -0400

    added custom logo done by seth tallman and edited some styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,9 +12,6 @@ const FooterPage = () => {
                     <a className="fb-ic"><i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
                     <a className="tw-ic"><i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
                     <a className="gplus-ic"><i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                    <a className="li-ic"><i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                    <a className="ins-ic"><i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
-                    <a className="pin-ic"><i className="fa fa-pinterest fa-lg white-text fa-2x"> </i></a>
                     </div>
                 </Col>
             </Row>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo_icon.png';
+import logo from './Tija-Full.png';
 import './Logo.css';
 
 const Logo = () => {
   return(
     <div className='container'>
-      <h1 className='underline mr5 mt4 float-right' id='title'>tija</h1>
-      <img src={logo} className='mt2 ml4' />
+      <div className='row'>
+        <div className='col d-flex justify-content-center'>
+          <img src={logo} className='logo mt2 ml4 mb3' alt='Colorful Hourglass Logo' />
+        </div>
+      </div>
     </div>
   );
 }
